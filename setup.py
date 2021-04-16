cd8f7b04bd8f45cc4c954e129c2754e1795c14e0
Shlomi Fish <shlomif@shlomifish.org>
2021-04-16 09:47:44 +0300

    prepare for a release

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
         re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
     ),
+    long_description_content_type='text/x-rst',
     author='Shlomi Fish',
     author_email='shlomif@cpan.org',
     url='https://github.com/shlomif/python-vnu_validator',
