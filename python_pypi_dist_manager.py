b8eed0a1fda2709a6460c808544ef63df1a9227d
Shlomi Fish <shlomif@shlomifish.org>
2021-12-20 11:15:56 +0200

    convert-to-pydistman

---
+++ python_pypi_dist_manager.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the terms of the MIT license.
+
+import sys
+
+from pydistman import DistManager
+
+
+class Derived(DistManager):
+    """docstring for Derived"""
+    pass
+
+
+try:
+    cmd = sys.argv.pop(1)
+except IndexError:
+    cmd = 'build'
+
+dist_name = "vnu-validator"
+
+obj = Derived(
+    dist_name=dist_name,
+    dist_version="0.10.0",
+    project_name=dist_name,
+    project_short_description="Python Wrapper for the v.Nu HTML Validator",
+    release_date="2021-12-20",
+    project_year="2020",
+    aur_email="shlomif@cpan.org",
+    project_email="shlomif@cpan.org",
+    full_name="Shlomi Fish",
+    github_username="shlomif",
+    filter_test_reqs=True,
+)
+obj.run_command(cmd=cmd, args=[])
